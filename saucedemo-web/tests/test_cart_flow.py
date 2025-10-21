007bd9fed982b0fec513eb6527304f0a7317f492
Rasul Logvin <leo.gordon.leo@gmail.com>
2025-10-21 22:35:11 +0500

    Update test_cart_flow.py

--- saucedemo-web/tests/test_cart_flow.py
+++ saucedemo-web/tests/test_cart_flow.py
@@ -1,1 +1,18 @@
+import pytest
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
 
+@pytest.mark.ui
+def test_add_to_cart_and_checkout_visible(page):
+    login = LoginPage(page)
+    inv = InventoryPage(page)
+
+    login.open()
+    login.login("standard_user", "secret_sauce")
+    page.wait_for_url("**/inventory.html")
+
+    inv.add_first_n_items(2)
+    assert inv.cart_count() == 2
+
+    inv.open_cart()
+    assert inv.can_checkout(), "Checkout button should be visible"
